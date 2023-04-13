4025137905b63e4482fd926a2ebf003d3c80b261
Алексей <55784938+aolychkin@users.noreply.github.com>
2023-04-13 16:03:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,4 +11,11 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.07
+    z_stat, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], value=0, alternative='smaller')
+    z_crit = np.abs(norm.ppf(alpha))
+    #print(f"Z_stat = {z_stat} \t Z_crit = {z_crit} \t P = {p_value}")
+    if np.abs(z_stat) > z_crit:
+        return True
+    else:
+        return False
